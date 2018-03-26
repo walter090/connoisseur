5902ae56053619251d7c745f49da31ccc07fa294
walter_wu090 <walter.wu090@gmail.com>
2018-03-26 00:30:29 -0400

    Add preprocessing

---
+++ preprocess.py
@@ -0,0 +1,24 @@
+import os
+
+
+def create_metadata(dataset_dir, out='metadata'):
+    composer_map = {}
+    for root, subdirs, files in os.walk(dataset_dir):
+        composer = root.split('/')[-1]
+        for file in files:
+            if composer in composer_map:
+                composer_map[composer].append(file)
+            else:
+                composer_map[composer] = [file]
+
+    if not os.path.isdir(out):
+        os.mkdir(out)
+
+    with open(os.path.join(out, 'composer_map.tsv'), 'w') as writer:
+        for composer, files in composer_map.items():
+            for file in files:
+                writer.write(composer + '   ' + file + '\n')
+
+
+if __name__ == '__main__':
+    create_metadata('/home/snowman/Documents/datasets/piano_midi')
